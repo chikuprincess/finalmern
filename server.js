aa84122e0e92547564c853c4e25b275f20c3dfe1
S.Priyavarshini <pvarshini0529@gmail.com>
2024-12-26 13:54:54 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from 'express'
+import userRouter from './routes/user.js'
+import recipeRouter from './routes/recipe.js'
+import cors from 'cors'
+const app = express(); 
+
+app.use(bodyParser.json())
+app.use(cors({
+  origin:true,
+  methods:["GET","POST","PUT","DELETE"],
+  credentials:true
+ 
+}))
+
+// userRouter
+app.use('/api',userRouter)
+
+// recipeRouter
+app.use('/api',recipeRouter)
+mongoose
+  .connect(
+    "mongodb+srv://pvarshini0529:APvtNBvP0yR9PT9G@cluster0.4209r.mongodb.net/",
+    {
+      dbName: "react",
+    }
+  )
+  .then(() => console.log("MongoDB is Connected..!"))
+  .catch((err) => console.log(err.message));
+
+const port = 3000;
+app.listen(port, () => console.log(`server is running on port ${port}`));
+
